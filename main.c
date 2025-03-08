d395d1a68e773a813ce05e6363595ac252b9a0c6
Fefe07 <felixlandreau97@gmail.com>
2025-03-08 15:21:03 +0100

    sudoku to k_cnf works !

--- main.c
+++ main.c
@@ -5,9 +5,9 @@
 #include <time.h>
 #include <math.h>
 
-int group(int i, int j);                        // ok
+int group(int i, int j);												// ok
 bool same_zone(int i1, int j1, int i2, int j2); // ok
-bool check(int **grid, int i, int j);           // ok
+bool check(int **grid, int i, int j);					 // ok
 
 void printGrid(int **grid); // ok
 
@@ -44,15 +44,15 @@ void solve_simple_notes_backtrack(int **grid, float *nb_tech);
 
 int **lecture(int n, char *nom_de_la_base);
 typedef struct {
-  float D_TO;
-  float D_TR;
-  int **grid;
+	float D_TO;
+	float D_TR;
+	int **grid;
 } grid_diffs;
 grid_diffs lecture_db_diffs(int n, char *nom_de_la_base);
 
 typedef struct {
-  int technique;
-  int *positions; // le contenu de ce tableau va varier suivant les fonctions
+	int technique;
+	int *positions; // le contenu de ce tableau va varier suivant les fonctions
 } tech_occurence;
 
 int **grid_of_string(char *s);
@@ -63,217 +63,258 @@ void updateNotes(int **grid, bool ***notes, int row, int col);
 
 void affiche_notes(bool ***notes);
 
+typedef struct var_s{
+    /* Une variable est de la forme p_i,j,k, elle indique si la case i,j contient k */
+    /* 0 <= i,j < 9 */
+    /* 1 <= k <= 9 */
+    int i;
+    int j;
+    int k;
+}var;
+
+typedef struct {
+    /* On représente un litéral par une variable (un entier) 
+    et une positivite (1 si litéral positif, 0 si litéral négatif)
+    On représente donc une clause par un tableau de litéraux */
+    int nb_lit ;
+    var* vars ;
+    bool* positif ;
+}clause;
+
+
+typedef struct {
+    /* Ce type est beaucoup moins général ; le filtre est spécifique à cet usage */
+    int nb_var ;
+    var* vars ;
+    bool* filtre ;
+}clause_1in9;
+
+
+struct k_cnf_s {
+    int m ; //nb_clauses
+    int k ; 
+    clause* clauses ;
+};
+typedef struct k_cnf_s* k_cnf; 
+
+k_cnf sudoku_to_cnf(int** grid);
+void print_k_cnf(k_cnf f);
+
+
 int group(int i, int j) { return 3 * (i / 3) + j / 3; }
 // renvoie le numéro du sous-groupe (de 0 à 8)
 
 bool same_zone(int i1, int j1, int i2, int j2) {
-  // i1, i2, j1, j2 entre 0 et 8
-  return (i1 == i2) || (j1 == j2) || (group(i1, j1) == group(i2, j2));
+	// i1, i2, j1, j2 entre 0 et 8
+	return (i1 == i2) || (j1 == j2) || (group(i1, j1) == group(i2, j2));
 } // même ligne ou même colonne ou même sous-groupe
 
 void print_tab_int(int *tab, int size) {
-  for (int i = 0; i < size; i++) {
-    printf("%d  ", tab[i]);
-  }
-  printf("\n");
+	for (int i = 0; i < size; i++) {
+		printf("%d	", tab[i]);
+	}
+	printf("\n");
 }
 void print_tab_float(float *tab, int size) {
-  for (int i = 0; i < size; i++) {
-    printf("%.4f,  ", tab[i]);
-  }
-  printf("\n");
+	for (int i = 0; i < size; i++) {
+		printf("%.4f,	", tab[i]);
+	}
+	printf("\n");
 }
 
 float calcule_cout(float *coeffs, float *coeffs_used, float **results, float *difficulties, int results_size);
 
 float *cree_coeffs() {
-  /* Tableau des techniques :
-  0 - nakedSingle
-  1 - hiddenSingle
-  2 - nakedPair
-  3 - nakedTriple
-  4 - hiddenPair
-  5 - hiddenTriple
-  6 - pointing Pair / Triple
-  7 - Box line reduction
-  8 - X-Wing
-  9 - Backtracking
-  */
-  float *coeffs = malloc(13 * sizeof(float));
-  assert(coeffs != NULL);
-  for(int i = 0; i<13; i++){
-    coeffs[i] = (rand() % 100) / 50. ; 
-  }
-  return coeffs;
+	/* Tableau des techniques :
+	0 - nakedSingle
+	1 - hiddenSingle
+	2 - nakedPair
+	3 - nakedTriple
+	4 - hiddenPair
+	5 - hiddenTriple
+	6 - pointing Pair / Triple
+	7 - Box line reduction
+	8 - X-Wing
+	9 - Backtracking
+	*/
+	float *coeffs = malloc(13 * sizeof(float));
+	assert(coeffs != NULL);
+	for(int i = 0; i<13; i++){
+		coeffs[i] = (rand() % 100) / 50. ; 
+	}
+	return coeffs;
 }
 
 float *cree_coeffs_used() {
-  float *coeffs_used = malloc(13 * sizeof(float));
-  assert(coeffs_used != NULL);
-  for(int i = 0; i<13; i++){
-    coeffs_used[i] = (rand() % 100 + 10) / 50. ;  
-  }
-  coeffs_used[2] = 0;
-  coeffs_used[0] = 0;
-  return coeffs_used;
+	float *coeffs_used = malloc(13 * sizeof(float));
+	assert(coeffs_used != NULL);
+	for(int i = 0; i<13; i++){
+		coeffs_used[i] = (rand() % 100 + 10) / 50. ;	
+	}
+	coeffs_used[2] = 0;
+	coeffs_used[0] = 0;
+	return coeffs_used;
 }
 
 int main() {
-  srand(time(NULL));
-  
-  float *coeffs = cree_coeffs();
+	srand(time(NULL));
+	float *coeffs = cree_coeffs();
+	float *coeffs_used = cree_coeffs_used();
 
-  float *coeffs_used = cree_coeffs_used();
+	int results_size = 300;
+	FILE *f = fopen("resultats.txt", "w");
 
-  int results_size = 300;
-  FILE *f = fopen("resultats.txt", "w");
+	/*
+	int **grid = grid_of_string("645010893738459621219638745597060184481975000326"
+															"841579902080010803190000164020908");
+	float *t = malloc(12 * sizeof(float));
+	for (int i = 0; i < 12; i++) {
+		t[i] = 0.;
+	}
+	solve_notes(grid, initialize_notes(grid), t);
+	printGrid(grid);
+	print_tab_float(t, 12);
+	*/
+	float average_dtr = 1.458184 ;
+	
+	float **results = malloc(results_size * sizeof(float *));
+	assert(results != NULL);
+	float *difficulties = malloc(results_size * sizeof(float));
+	assert(difficulties != NULL);
 
-  /*
-  int **grid = grid_of_string("645010893738459621219638745597060184481975000326"
-                              "841579902080010803190000164020908");
-  float *t = malloc(12 * sizeof(float));
-  for (int i = 0; i < 12; i++) {
-    t[i] = 0.;
-  }
-  solve_notes(grid, initialize_notes(grid), t);
-  printGrid(grid);
-  print_tab_float(t, 12);
-  */
-  float average_dtr = 1.458184 ;
-  
-  float **results = malloc(results_size * sizeof(float *));
-  assert(results != NULL);
-  float *difficulties = malloc(results_size * sizeof(float));
-  assert(difficulties != NULL);
+	for (int nbGrille = 0; nbGrille < results_size ; nbGrille++) {
 
-  for (int nbGrille = 2; nbGrille <= results_size + 1; nbGrille++) {
+		printf("Grille n %d\n", nbGrille);
+		grid_diffs g = lecture_db_diffs(nbGrille+2, "grilles/Bases_de_donnees_evaluees.csv");
+		//int** g2 = lecture(nbGrille, "grilles/top50000.txt");
+		
+		//printGrid(g.grid);
+		difficulties[nbGrille] = (g.D_TR-average_dtr)* (g.D_TR-average_dtr);
 
-    printf("Grille n %d\n", nbGrille);
-    grid_diffs g = lecture_db_diffs(nbGrille, "grilles/Bases_de_donnees_evaluees.csv");
-    //int** g2 = lecture(nbGrille, "grilles/top50000.txt");
-    
-    //printGrid(g.grid);
-    difficulties[nbGrille - 2] = (g.D_TR-average_dtr)* (g.D_TR-average_dtr);
+		float *nb_tech = malloc(13 * sizeof(float));
+		for (int i = 0; i < 13; i++) {
+			nb_tech[i] = 0.;
+		}
+		//printGrid(g.grid);
+		solve_simple_notes_backtrack(g.grid, nb_tech);
+		// print_tab_int(nb_tech, 10);
+		results[nbGrille] = nb_tech;
+		for (int i = 0; i < 13; i++) {
+			fprintf(f, "%.4f ; ", nb_tech[i]);
+		}
+		fprintf(f, "%f ; %f ;\n", g.D_TO, g.D_TR);
+		free_grid(g.grid);
+	}
+	fclose(f);
+	printf("Coucou\n");
 
-    float *nb_tech = malloc(13 * sizeof(float));
-    for (int i = 0; i < 13; i++) {
-      nb_tech[i] = 0.;
-    }
-    //printGrid(g.grid);
-    solve_simple_notes_backtrack(g.grid, nb_tech);
-    // print_tab_int(nb_tech, 10);
-    results[nbGrille - 2] = nb_tech;
-    for (int i = 0; i < 13; i++) {
-      fprintf(f, "%.4f ; ", nb_tech[i]);
-    }
-    fprintf(f, "%f ; %f ;\n", g.D_TO, g.D_TR);
-    free_grid(g.grid);
-  }
-  fclose(f);
-  printf("Coucou\n");
+	float cout = calcule_cout(coeffs, coeffs_used, results, difficulties, results_size);
+	float ancien_cout = cout + 42;
+	int i = 0;
+	while (ancien_cout > cout) {
+		
+		ancien_cout = cout ;
+		for (int j = 0; j <13; j++) {
+			//for (int k = 0; k < 100; k++) {
+				coeffs[j] *= 0.99;
+				float nouveau_cout = calcule_cout(coeffs, coeffs_used, results, difficulties, results_size);
+				if (nouveau_cout > cout) {
+					coeffs[j] /= 0.99;
+				} else {
+					cout = nouveau_cout;
+				}
+				coeffs[j] *= 1.01;
+				nouveau_cout = calcule_cout(coeffs, coeffs_used, results, difficulties, results_size);
+				if (nouveau_cout >= cout) {
+					coeffs[j] /= 1.01;
+				} else {
+					cout = nouveau_cout;
+				}
+				coeffs_used[j] *= 0.99;
+				nouveau_cout = calcule_cout(coeffs, coeffs_used, results, difficulties, results_size);
+				if (nouveau_cout > cout) {
+					coeffs_used[j] /= 0.99;
+				} else {
+					cout = nouveau_cout;
+				}
+				coeffs_used[j] *= 1.01;
+				nouveau_cout = calcule_cout(coeffs, coeffs_used, results, difficulties, results_size);
+				if (nouveau_cout >= cout) {
+					coeffs_used[j] /= 1.01;
+				} else {
+					cout = nouveau_cout;
+				}
+			//}
+		}
+		if(i%100 == 0){
+			printf("i = %d : \n", i);
+			print_tab_float(coeffs, 13);
+			print_tab_float(coeffs_used, 13);
+			printf("Cout = %f\n", cout);
+			//float ec	= sqrt(cout);
+			//printf("ecart-type = %f\n", ec);
+			fflush(stdout);
+		}
+		i++;
+	}
 
-  float cout = calcule_cout(coeffs, coeffs_used, results, difficulties, results_size);
-  float ancien_cout = cout + 42;
-  int i = 0;
-  while (ancien_cout > cout) {
-    
-    ancien_cout = cout ;
-    for (int j = 0; j <13; j++) {
-      //for (int k = 0; k < 100; k++) {
-        coeffs[j] *= 0.99;
-        float nouveau_cout = calcule_cout(coeffs, coeffs_used, results, difficulties, results_size);
-        if (nouveau_cout > cout) {
-          coeffs[j] /= 0.99;
-        } else {
-          cout = nouveau_cout;
-        }
-        coeffs[j] *= 1.01;
-        nouveau_cout = calcule_cout(coeffs, coeffs_used, results, difficulties, results_size);
-        if (nouveau_cout >= cout) {
-          coeffs[j] /= 1.01;
-        } else {
-          cout = nouveau_cout;
-        }
-        coeffs_used[j] *= 0.99;
-        nouveau_cout = calcule_cout(coeffs, coeffs_used, results, difficulties, results_size);
-        if (nouveau_cout > cout) {
-          coeffs_used[j] /= 0.99;
-        } else {
-          cout = nouveau_cout;
-        }
-        coeffs_used[j] *= 1.01;
-        nouveau_cout = calcule_cout(coeffs, coeffs_used, results, difficulties, results_size);
-        if (nouveau_cout >= cout) {
-          coeffs_used[j] /= 1.01;
-        } else {
-          cout = nouveau_cout;
-        }
-      //}
-    }
-    if(i%100 == 0){
-      printf("i = %d : \n", i);
-      print_tab_float(coeffs, 13);
-      print_tab_float(coeffs_used, 13);
-      printf("Cout = %f\n", cout);
-      //float ec  = sqrt(cout);
-      //printf("ecart-type = %f\n", ec);
-      fflush(stdout);
-    }
-    i++;
-  }
+	int** grid42 = grid_of_string("081600090000000000004037600600400500030000070007002004005210300000000000070004810");
+	k_cnf g = sudoku_to_cnf(grid42);
+	print_k_cnf(g); 
+	printGrid(grid42);
 
-  /*
-  for(int nbGrille = 2; nbGrille<=2; nbGrille++){
-      difficulty = 0;
-      int** grid42 = lecture(nbGrille);
-      printGrid(grid42);
+	/*
+	for(int nbGrille = 2; nbGrille<=2; nbGrille++){
+			difficulty = 0;
+			int** grid42 = lecture(nbGrille);
+			printGrid(grid42);
 
-      bool finished ;
-      finished = solve(grid42);
-      //printf("%d\n", finished);
-      printGrid(grid42);
-      if(finished==false){
-          printf(" on utilise les notes\n");
-          bool ***notes = createNotes();
-          // notes[i][j][k] == true si la case ij peut accueillir l'indice k+1
+			bool finished ;
+			finished = solve(grid42);
+			//printf("%d\n", finished);
+			printGrid(grid42);
+			if(finished==false){
+					printf(" on utilise les notes\n");
+					bool ***notes = createNotes();
+					// notes[i][j][k] == true si la case ij peut accueillir l'indice k+1
 
-          for (int i = 0; i < 9; i++) {
-              for (int j = 0; j < 9; j++) {
-                  updateNotes(grid42, notes, i, j);
-              }
-          }
+					for (int i = 0; i < 9; i++) {
+							for (int j = 0; j < 9; j++) {
+									updateNotes(grid42, notes, i, j);
+							}
+					}
 
 
-          finished = solve_notes(grid42, notes, nb_tech);
-          printGrid(grid42);
-          print_tab(nb_tech, 10);
-          // si les techniques ne suffisent pas,
-          // on passe au backtracking
-          if (!finished){
-              finished = backtrack(grid42, notes, nb_tech);
-              assert(finished);
-          }
-          //printGrid(grid42);
-      }
-      print_tab(nb_tech, 10);
-      float diff = 0;
-      for(int i = 0; i<10; i++){
-          diff += nb_tech[i] * coeffs[i] ;
-      }
-      //printGrid(grid42);
-      //printf("Fini !\n");
-      printf("nbGrille = %d; difficulty = %.1f\n", nbGrille, diff);
-  }
-  */
-  //printGrid(lecture_db_diffs(344, "grilles/Bases_de_donnees_evaluees.csv").grid);
+					finished = solve_notes(grid42, notes, nb_tech);
+					printGrid(grid42);
+					print_tab(nb_tech, 10);
+					// si les techniques ne suffisent pas,
+					// on passe au backtracking
+					if (!finished){
+							finished = backtrack(grid42, notes, nb_tech);
+							assert(finished);
+					}
+					//printGrid(grid42);
+			}
+			print_tab(nb_tech, 10);
+			float diff = 0;
+			for(int i = 0; i<10; i++){
+					diff += nb_tech[i] * coeffs[i] ;
+			}
+			//printGrid(grid42);
+			//printf("Fini !\n");
+			printf("nbGrille = %d; difficulty = %.1f\n", nbGrille, diff);
+	}
+	*/
+	//printGrid(lecture_db_diffs(344, "grilles/Bases_de_donnees_evaluees.csv").grid);
 
-  free(coeffs);
-  free(coeffs_used);
-  for(int i = 0; i<results_size; i++){
-    free(results[i]);
-  }
-  free(results);
-  free(difficulties);
+	free(coeffs);
+	free(coeffs_used);
+	for(int i = 0; i<results_size; i++){
+		free(results[i]);
+	}
+	free(results);
+	free(difficulties);
 
-  return 0;
+	return 0;
 }
---
+++ sudoku_to_cnf.c
@@ -0,0 +1,294 @@
+#include<stdlib.h>
+#include<assert.h>
+#include<stdbool.h>
+#include<stdio.h>
+
+typedef struct var_s{
+    /* Une variable est de la forme p_i,j,k, elle indique si la case i,j contient k */
+    /* 0 <= i,j < 9 */
+    /* 1 <= k <= 9 */
+    int i;
+    int j;
+    int k;
+}var;
+
+typedef struct {
+    /* On représente un litéral par une variable (un entier) 
+    et une positivite (1 si litéral positif, 0 si litéral négatif)
+    On représente donc une clause par un tableau de litéraux */
+    int nb_lit ;
+    var* vars ;
+    bool* positif ;
+}clause;
+
+
+typedef struct {
+    /* Ce type est beaucoup moins général ; le filtre est spécifique à cet usage */
+    int nb_var ;
+    var* vars ;
+    bool* filtre ;
+}clause_1in9;
+
+
+struct k_cnf_s {
+    int m ; //nb_clauses
+    int k ; 
+    clause* clauses ;
+};
+typedef struct k_cnf_s* k_cnf; 
+
+
+void free_clause(clause c){
+    free(c.vars);
+    free(c.positif);
+}
+
+void free_clause_1in9(clause_1in9 c){
+    free(c.vars);
+    free(c.filtre);
+}
+
+void print_k_cnf(k_cnf f){
+    printf("m = %d\n",f->m);
+    printf("k = %d\n", f->k);
+    for(int a = 0; a<f->m; a++){
+        for(int b = 0; b<f->clauses[a].nb_lit;b++){
+            if(f->clauses[a].positif[b]){
+                printf("x_%d,%d,%d ", f->clauses[a].vars[b].i, f->clauses[a].vars[b].j, f->clauses[a].vars[b].k);
+            }
+            else{
+                printf("not x_%d,%d,%d ", f->clauses[a].vars[b].i, f->clauses[a].vars[b].j, f->clauses[a].vars[b].k);
+            }
+            if(b<f->clauses[a].nb_lit-1){
+                printf("or ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+
+k_cnf sudoku_to_cnf(int** grid){
+    /* génère une cnf à partir d'une grille de sudoku */
+    /* La tactique qui consiste à encoder les règles puis les indices ne fonctionne pas
+    car un sudoku plus compliqué aurait donc moins de contraintes pour le même nombre d'indices... 
+    et de toute façon cela riviendrait juste à compter les indices initiaux */
+
+    clause_1in9* clauses_1in9 = malloc(324*sizeof(clause_1in9));
+    assert(clauses_1in9!=NULL);
+
+    /* La transformation se fera donc en trois étapes : 
+    1 - écriture des clauses 1-dans-9 imposées par les règles (324 au total = 81 pour les lignes, colonnes, zones et cases)
+    2 - élagage de ces clauses : chaque indice donné nous permet 
+        - de supprimer 4 clauses (celles ou la variable mise à vrai apparait)
+        - de supprimer toutes les autres variables de la ligne, colonne, zone et tour de l'indice
+        il y a au plus 28 variables à retirer des clauses restantes
+    on ne considère pas que les clauses réduites à une variable forment de nouveaux indices,
+    car cela correspondrait à dérouler les naked single et hidden single */
+    /* Remarque : la fusion de ces deux étapes permettrait de gagner en efficacité, mais perdrait en clarté */
+
+    /* Étape 1 :*/
+    /* Cases */
+    for(int i = 0; i<9; i++){
+        for(int j = 0; j<9; j++){
+            var* vars = malloc(9*sizeof(var));
+            assert(vars!=NULL);
+            bool* filtre = malloc(9*sizeof(bool));
+            assert(filtre!=NULL);
+            for(int k = 0; k<9; k++){
+                filtre[k] = true;
+                var v = {.i = i, .j=j, .k=k};
+                vars[k] = v;
+            }
+            clause_1in9 c = {.nb_var = 9, .vars = vars, .filtre = filtre};
+            clauses_1in9[9*i+j] = c;
+        }
+    }
+
+    /* Lignes */
+    for(int i = 0; i<9; i++){
+        for(int k = 0; k<9; k++){
+            var* vars = malloc(9*sizeof(var));
+            assert(vars!=NULL);
+            bool* filtre = malloc(9*sizeof(bool));
+            assert(filtre!=NULL);
+            for(int j = 0; j<9; j++){
+                filtre[j] = true;
+                var v = {.i = i, .j=j, .k=k};
+                vars[j] = v;
+            }
+            clause_1in9 c = {.nb_var = 9, .vars = vars, .filtre = filtre};
+            clauses_1in9[81 + 9*i+k] = c;
+        }
+    }
+
+    /* Colonnes */
+    for(int j = 0; j<9; j++){
+        for(int k = 0; k<9; k++){
+            var* vars = malloc(9*sizeof(var));
+            assert(vars!=NULL);
+            bool* filtre = malloc(9*sizeof(bool));
+            assert(filtre!=NULL);
+            for(int i = 0; i<9; i++){
+                filtre[i] = true;
+                var v = {.i = i, .j=j, .k=k};
+                vars[i] = v;
+            }
+            clause_1in9 c = {.nb_var = 9, .vars = vars, .filtre = filtre};
+            clauses_1in9[162 + 9*j+k] = c;
+        }
+    }
+
+    /* Zones */
+    for(int z = 0; z<9; z++){
+        for(int k = 0; k<9; k++){
+            var* vars = malloc(9*sizeof(var));
+            assert(vars!=NULL);
+            bool* filtre = malloc(9*sizeof(bool));
+            assert(filtre!=NULL);
+            for(int c = 0; c<9; c++){
+                filtre[c] = true;
+                var v = {.i = 3*(z/3)+c/3, .j=3*(z%3)+c%3, .k=k};
+                vars[c] = v;
+            }
+            clause_1in9 cl = {.nb_var = 9, .vars = vars, .filtre = filtre};
+            clauses_1in9[243 + 9*z+k] = cl;
+        }
+    }
+
+
+    
+    /* Étape 2 : */
+    bool* filtre = malloc(324*sizeof(bool));
+    assert(filtre!=NULL);
+    for(int i = 0; i<324; i++){
+        filtre[i] = true ;
+    }
+    /* On ne peut pas retirer les éléments du tableau à proprement parler, donc on filtre */
+
+    
+    for(int i = 0; i<9; i++){
+        for(int j = 0; j<9; j++){
+            if(grid[i][j]>0){
+                int k = grid[i][j] -1 ;
+                int z = 3*(i/3) + j/3 ;
+                int c = 3*(i%3) + j%3 ;
+                filtre[9*i+j] = false ;
+                filtre[81 + 9*i+k] = false ;
+                filtre[162 + 9*j+k] = false ;
+                filtre[243 + 9*z+k] = false ;
+
+                // on retire ces variables
+                for(int k2 = 0; k2<9; k2++){
+                    clauses_1in9[81 + 9*i+k2].filtre[j] = false ;
+                    clauses_1in9[162 + 9*j+k2].filtre[i] = false ;
+                    clauses_1in9[243 + 9*z+k2].filtre[c] = false ;
+                    
+                }
+                for(int i2 = 0; i2<9; i2++){
+                    clauses_1in9[9*i2+j].filtre[k] = false ;
+                    clauses_1in9[81 + 9*i2+k].filtre[j] = false ;
+                    int z2 = 3*(i2/3)+j/3 ;
+                    int c2 = 3*(i2%3)+(j%3) ;
+                    assert(z2<9);
+                    assert(c2<9) ;
+                    clauses_1in9[243 + 9*z2+k].filtre[c2] = false ;
+                }
+                for(int j2 = 0; j2<9; j2++){
+                    clauses_1in9[9*i+j2].filtre[k] = false ;
+                    clauses_1in9[162 + 9*j2+k].filtre[i] = false ;
+                    clauses_1in9[243 + 9*(3*(i/3)+j2/3)+k].filtre[3*(i%3)+j2%3] = false ;
+                }
+                for(int c2 = 0; c2<9; c2++){
+                    int i2 = 3*(z/3)+c2/3 ;
+                    int j2 = 3*(z%3)+c2%3 ;
+                    clauses_1in9[9*i2+j2].filtre[k] = false ;
+                    clauses_1in9[162 + 9*i2+k].filtre[j2] = false ;
+                    clauses_1in9[162 + 9*j2+k].filtre[i2] = false ;
+                    
+                }
+            }
+        }
+    }
+    
+    
+    /* On crée la k_cnf */
+
+    int m = 0; //nb_clauses
+    
+    // tableau pouvant stocker le nombre maximal de clauses(sudoku vide)
+    // il sera redimensionné
+    clause* clauses = malloc(324*37*sizeof(clause));
+    assert(clauses!=NULL);
+
+    
+
+    for(int a = 0; a<324; a++){
+        if(filtre[a]){
+            int nb_lit = 0;
+            for(int b = 0; b<9; b++){
+                if(clauses_1in9[a].filtre[b]){
+                    nb_lit++ ;
+                }
+            }
+            // si il y a au moins une variable
+            if(nb_lit >0){
+                var* vars = malloc(nb_lit*sizeof(var));
+                assert(vars!=NULL);
+                bool* positif = malloc(nb_lit*sizeof(bool));
+                assert(positif!=NULL);
+                int i = 0;
+                for(int b = 0; b<9; b++){
+                    if(clauses_1in9[a].filtre[b]){
+                        vars[i] = clauses_1in9[a].vars[b] ;
+                        positif[i] = true ;
+                        i++ ;
+                    }
+                }
+                clause c = {.vars = vars, .nb_lit = nb_lit, .positif = positif} ;
+                clauses[m] = c;
+                m++;
+                
+                for(int i = 0; i<nb_lit; i++){
+                    for(int j = 0; j<nb_lit; j++){
+                        if(i<j){
+                            var* vars_2 = malloc(2*sizeof(var));
+                            assert(vars_2!=NULL);
+                            bool* pos_2 = malloc(2*sizeof(bool));
+                            assert(pos_2!=NULL);
+                            vars_2[0] = vars[i];
+                            vars_2[1] = vars[j];
+                            pos_2[0] = false ;
+                            pos_2[1] = false ;
+                            clause c_2 = {.nb_lit=2, .positif=pos_2, .vars=vars_2} ;
+                            clauses[m] = c_2;
+                            m++ ;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    clause* clauses_redimensionne = malloc(m*sizeof(clause));
+    assert(clauses_redimensionne!=NULL);
+    for(int i = 0; i<m; i++){
+        clauses_redimensionne[i]=clauses[i];
+    }
+
+    // /* Mettre des free ! */
+    // for(int i = 0; i<m; i++){
+    //     free_clause(clauses[i]);
+    // }
+    // free(clauses);
+    // for(int i =0; i<324; i++){
+    //     free_clause_1in9(clauses_1in9[i]);
+    // }
+    // free(clauses_1in9);
+
+    int k = 9;
+    k_cnf f = malloc(sizeof(struct k_cnf_s));
+    f->m = m;
+    f->k = k;
+    f->clauses = clauses_redimensionne ;
+    return f; 
+}
